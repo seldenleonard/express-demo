05102e593714754d057e0c24eeb551bb6ddaa914
seldenleonard <selden222@gmail.com>
2020-12-14 01:33:40 -0500

    adds delete method

--- index.js
+++ index.js
@@ -68,6 +68,16 @@ function validateCourse(course) {
 }
 
 
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found');
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
