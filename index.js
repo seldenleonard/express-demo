99b38334b81715e54575b42f966010c02751c6ee
seldenleonard <selden222@gmail.com>
2020-12-14 00:46:36 -0500

    updates error handling

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ app.post('/api/courses', (req, res) => {
     name: Joi.string().min(3).required()
   };
 
-  const result = Joi.ValidationError(req.body, schema);
+  const result = Joi.validate(req.body, schema);
 
   if (result.error) {
-    res.status(400).send(result.error).details[0].message;
+    res.status(400).send(result.error);
     return;
   }
   const course = {
@@ -39,8 +39,10 @@ app.post('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send('The course with the given ID was not found');
-  res.send(course);
+  if (!course) {
+    res.status(404).send('The course with the given ID was not found');
+    res.send(course);
+  }
 });
 
 const port = process.env.PORT || 3000;
